2085a3394c4294d72942b5d7f383f67c5be1237c
Stoyan Georgiev <98672346+stoyandg@users.noreply.github.com>
2023-02-06 15:49:00 +0200

    Update status.sh

--- modules/codebuild/status.sh
+++ modules/codebuild/status.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $CODEBUILD_BUILD_SUCCEEDING = 0]; then
+if [ $CODEBUILD_BUILD_SUCCEEDING = 0 ]; then
     echo "Build has failed!"
 else
     echo "Build has been successfully deployed!"
