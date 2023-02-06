720a3dd42ac863a3fda8377b2ec7b3dd836a285a
Stoyan Georgiev <98672346+stoyandg@users.noreply.github.com>
2023-02-06 13:11:06 +0200

    Update deploy.sh

--- modules/codebuild/deploy.sh
+++ modules/codebuild/deploy.sh
@@ -2,5 +2,5 @@
 
 cd ${CODEBUILD_SRC_DIR}
 terraform init
-terraform plan -target=module.cluster -no-color -input=false -out exported_plan.out
+terraform plan -target=module.cluster --var-file="./modules/cluster/terraform.tfvars" -var="image_tag=${TAG}" -no-color -input=false -out exported_plan.out
 terraform apply -auto-approve -no-color -input=false exported_plan.out
