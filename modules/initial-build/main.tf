3f183263927d58ad31a0d24e04ca43a226e83eed
Stoyan Georgiev <98672346+stoyandg@users.noreply.github.com>
2023-02-06 12:28:45 +0200

    Add files via upload

--- modules/initial-build/main.tf
+++ modules/initial-build/main.tf
@@ -1,1 +1,6 @@
-
+resource "null_resource" "initial_building" {
+    provisioner "local-exec" {
+        command = "make build"
+        working_dir = var.working_directory
+    }
+}
---
+++ modules/initial-build/variables.tf
@@ -0,0 +1,5 @@
+variable "working_directory" {
+    description = "This is the directory in which we execute the make command"
+    type = string
+    default = "/home/stoyan/terraform/demo2foreach/application"
+}
