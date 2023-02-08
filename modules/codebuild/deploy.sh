61230d860ccf2a6ebb6bc0d8533304b312a957e7
Stoyan Georgiev <98672346+stoyandg@users.noreply.github.com>
2023-02-08 15:07:22 +0200

    Update deploy.sh

--- modules/codebuild/deploy.sh
+++ modules/codebuild/deploy.sh
@@ -4,3 +4,7 @@ cd ${CODEBUILD_SRC_DIR}
 terraform init
 terraform plan -target=module.cluster --var-file="./modules/cluster/terraform.tfvars" -var="image_tag=${TAG}" -no-color -input=false -out exported_plan.out
 terraform apply -auto-approve -no-color -input=false exported_plan.out
+
+# Initiate terraform 
+# Plan to apply module - Cluster with terraform.tfvars as variable file and set the variable image_tag to be using the environment variable ${TAG}
+# Apply the plan.
